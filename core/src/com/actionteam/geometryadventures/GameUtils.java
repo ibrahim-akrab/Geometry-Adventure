697df6b780660573bbb7e968fb9b1a6d9d8b0b0b
Ahmed Essam <theartful.ae@gmail.com>
2018-04-10 08:07:27 +0200

    New map

--- core/src/com/actionteam/geometryadventures/GameUtils.java
+++ core/src/com/actionteam/geometryadventures/GameUtils.java
@@ -62,6 +62,7 @@ public abstract class GameUtils {
 
     public ECSManager loadLevel(String levelName){
         Map map = loadMap(levelName);
+        map.updateTiles();
         aiUtils = new AIUtils(map);
         ECSManager ecsManager = new ECSManager();
 
@@ -99,6 +100,56 @@ public abstract class GameUtils {
             ecsManager.addComponent(collisionComponent, entity);
         }
 
+        for(Tile floorTile : map.getMiscTiles()){
+            int entity = ecsManager.createEntity();
+            PhysicsComponent physicsComponent = new PhysicsComponent();
+            GraphicsComponent graphicsComponent = new GraphicsComponent();
+
+            physicsComponent.position.set(floorTile.x, floorTile.y);
+            graphicsComponent.textureName = floorTile.textureName;
+            graphicsComponent.textureIndex = floorTile.textureIndex;
+
+            ecsManager.addComponent(physicsComponent, entity);
+            ecsManager.addComponent(graphicsComponent, entity);
+        }
+
+
+        for(Tile enemyTile : map.getEnemyTiles()) {
+            // temporary, for enemy creation.
+            int enemyEntity = ecsManager.createEntity();
+            GraphicsComponent enemyGC = new GraphicsComponent();
+            enemyGC.textureName = enemyTile.textureName;
+            enemyGC.textureIndex = 0;
+            enemyGC.height = 1;
+            enemyGC.width = 1;
+            CollisionComponent enemyCC = new CollisionComponent();
+            enemyCC.shapeType = CollisionComponent.RECTANGLE;
+            enemyCC.width = 0.7f;
+            enemyCC.height = 0.7f;
+            enemyCC.radius = 0.7f;
+            enemyCC.id = 0;
+            enemyCC.mask = ~0;
+            PhysicsComponent enemyPC = new PhysicsComponent();
+            enemyPC.position.x = enemyTile.x;
+            enemyPC.position.y = enemyTile.y;
+
+            EnemyComponent enemyComponent = new EnemyComponent();
+            /*
+            // The enemy's path. Automate this!!
+            Float[] v1 = new Float[] {5.0f, 5.0f, 0.0f, 3.0f};
+            Float[] v2 = new Float[] {5.0f, 8.0f, 30.0f, 2.0f};
+            Float[] v3 = new Float[] {3.0f, 7.0f, -10.0f, 1.0f};
+            enemyComponent.pathPoints.add(v1);
+            enemyComponent.pathPoints.add(v2);
+            enemyComponent.pathPoints.add(v3);
+            enemyComponent.remainingTime = (enemyComponent.pathPoints.get(0))[3].floatValue();
+            */
+            ecsManager.addComponent(enemyPC, enemyEntity);
+            ecsManager.addComponent(enemyGC, enemyEntity);
+            ecsManager.addComponent(enemyCC, enemyEntity);
+            ecsManager.addComponent(enemyComponent, enemyEntity);
+        }
+
         // temporary
         int entity = ecsManager.createEntity();
         GraphicsComponent gc = new GraphicsComponent();
@@ -114,12 +165,6 @@ public abstract class GameUtils {
         col.shapeType = CollisionComponent.RECTANGLE;
         col.id = 0;
         col.mask = ~0;
-        cc.leftBigCircleRadius = (float) (0.04 * Math.sqrt(Gdx.graphics.getWidth() *
-                Gdx.graphics.getWidth() + Gdx.graphics.getHeight() * Gdx.graphics.getHeight()));
-        cc.rightBigCircleRadius = (float) (0.18 * Math.sqrt(Gdx.graphics.getWidth() *
-                Gdx.graphics.getWidth() + Gdx.graphics.getHeight() * Gdx.graphics.getHeight()));
-        cc.maximumSpeed = 3.5f;
-
 
         WeaponComponent wc = WeaponFactory.createWeapon(WeaponComponent.RIOT_GUN);
 
@@ -128,41 +173,6 @@ public abstract class GameUtils {
         ecsManager.addComponent(gc, entity);
         ecsManager.addComponent(col, entity);
         ecsManager.addComponent(wc, entity);
-
-        for(int i = 0; i < 1; i++)
-        {
-            // temporary, for enemy creation.
-            int enemyEntity = ecsManager.createEntity();
-            GraphicsComponent enemyGC = new GraphicsComponent();
-            enemyGC.textureName = "man";
-            enemyGC.textureIndex = -1;
-            enemyGC.height = 1;
-            enemyGC.width = 1;
-            CollisionComponent enemyCC = new CollisionComponent();
-            enemyCC.shapeType = CollisionComponent.RECTANGLE;
-            enemyCC.width = 0.7f;
-            enemyCC.height = 0.7f;
-            enemyCC.radius = 0.7f;
-            enemyCC.id = 0;
-            enemyCC.mask = ~0;
-            PhysicsComponent enemyPC = new PhysicsComponent();
-            enemyPC.position.x = 5.01f;
-            enemyPC.position.y = 5.01f;
-
-            EnemyComponent enemyComponent = new EnemyComponent();
-            // The enemy's path. Automate this!!
-            Float[] v1 = new Float[] {5.0f, 5.0f, 0.0f, 3.0f};
-            Float[] v2 = new Float[] {5.0f, 8.0f, 30.0f, 2.0f};
-            Float[] v3 = new Float[] {3.0f, 7.0f, -10.0f, 1.0f};
-            enemyComponent.pathPoints.add(v1);
-            enemyComponent.pathPoints.add(v2);
-            enemyComponent.pathPoints.add(v3);
-            enemyComponent.remainingTime = (enemyComponent.pathPoints.get(0))[3].floatValue();
-            ecsManager.addComponent(enemyPC, enemyEntity);
-            ecsManager.addComponent(enemyGC, enemyEntity);
-            ecsManager.addComponent(enemyCC, enemyEntity);
-            ecsManager.addComponent(enemyComponent, enemyEntity);
-        }
 
         // create systems
         GraphicsSystem graphicsSystem = new GraphicsSystem(this);
