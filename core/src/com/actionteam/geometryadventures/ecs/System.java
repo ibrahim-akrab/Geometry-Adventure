0107cd388a6e292f74fbebe9afdd7cc239ce022f
Ibrahim-Akrab <ibrahim.m.akrab@gmail.com>
2018-05-03 14:51:50 +0200

    remove a comment from system class

--- core/src/com/actionteam/geometryadventures/ecs/System.java
+++ core/src/com/actionteam/geometryadventures/ecs/System.java
@@ -32,7 +32,6 @@ public abstract class System {
         }
         componentsMask = tmpMask;
         entities = new ArrayList<Integer>();
-//        setEcsManager();
     }
 
     void setEcsManager() {
