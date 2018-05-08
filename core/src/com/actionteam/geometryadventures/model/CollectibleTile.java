239dd9cd11f862058f12b44ae569518e301cf9da
rka <enrev91@gmail.com>
2018-05-09 00:29:15 +0200

    collectibleTile file added

---
+++ core/src/com/actionteam/geometryadventures/model/CollectibleTile.java
@@ -0,0 +1,10 @@
+package com.actionteam.geometryadventures.model;
+
+/**
+ * Created by rka97 on 5/8/2018.
+ */
+
+public class CollectibleTile extends Tile {
+    public int value;
+    public int type;
+}
