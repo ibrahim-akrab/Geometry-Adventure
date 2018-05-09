60628c8db18aecfeeb2cd152f61ce1a25067d351
Ibrahim-Akrab <ibrahim.m.akrab@gmail.com>
2018-05-09 23:35:44 +0200

    Add release notes

---
+++ core/src/test/java/com/actionteam/geometryadventures/systems/HealthSystemTest.java
@@ -0,0 +1,34 @@
+package com.actionteam.geometryadventures.systems;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ibrahim M. Akrab on 5/9/18.
+ * ibrahim.m.akrab@gmail.com
+ */
+public class HealthSystemTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void handle() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void ecsManagerAttached() {
+    }
+}
